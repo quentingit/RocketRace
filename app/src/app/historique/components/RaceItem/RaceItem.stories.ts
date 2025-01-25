58c7c9097a7d9936c82775557c81a59d26d4f866
quentingit <quentin.beranger@gmail.com>
2025-01-25 02:44:34 +0100

    fix(stories): fix stories and check github action

--- app/src/app/historique/components/RaceItem/RaceItem.stories.ts
+++ app/src/app/historique/components/RaceItem/RaceItem.stories.ts
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 import RaceItem from "./RaceItem";
-import { Race, RocketProgress } from "src/__generated__/graphql";
+import { Race, Rocket } from "src/__generated__/graphql";
 import { RocketInteraction } from "@appTypes/enrichedTypes";
 
 const meta: Meta<typeof RaceItem> = {
@@ -14,23 +14,25 @@ export default meta;
 type Story = StoryObj<typeof RaceItem>;
 
 // Mock data for rockets
-const mockRocket1: RocketInteraction = {
+const mockRocket1: Rocket = {
   id: "rocket-1",
   name: "Falcon 9",
   image: "/rocket.png",
+  description: "",
 };
 
-const mockRocket2: RocketInteraction = {
+const mockRocket2: Rocket = {
   id: "rocket-2",
   name: "Starship",
   image: "/rocket.png",
+  description: "",
 };
 
 // Mock data for race
 const mockRace: Race = {
   id: "race-123",
-  rocket1: mockRocket1 as RocketProgress,
-  rocket2: mockRocket2 as RocketProgress,
+  rocket1: mockRocket1 as RocketInteraction,
+  rocket2: mockRocket2 as RocketInteraction,
   winner: "rocket-1",
 };
 
