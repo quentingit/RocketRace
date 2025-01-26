b6d5680a1faf5e0a6c3ef0d3e84ad29757a3f8cf
quentingit <quentin.beranger@gmail.com>
2025-01-26 19:54:28 +0100

    chore: update config

--- app/next.config.ts
+++ app/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig = {
         protocol: process.env.NEXT_PUBLIC_GRAPHQL_PROTOCOL || "http",
         hostname: process.env.NEXT_PUBLIC_GRAPHQL_HOSTNAME || "localhost",
         port: process.env.NEXT_PUBLIC_GRAPHQL_PORT || "4000",
-        pathname: "/static/**",
+        pathname: "/**",
       },
     ],
   },
