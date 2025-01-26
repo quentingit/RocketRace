ba3757f80a5adeef294aeaccadba08b767b87669
quentingit <quentin.beranger@gmail.com>
2025-01-26 19:43:21 +0100

    chore : Dockerfile for develoment and production

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: true,
-  },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: process.env.NEXT_PUBLIC_GRAPHQL_PROTOCOL || "http",
         hostname: process.env.NEXT_PUBLIC_GRAPHQL_HOSTNAME || "localhost",
         port: process.env.NEXT_PUBLIC_GRAPHQL_PORT || "4000",
-        pathname: "/**",
+        pathname: "/static/**",
       },
     ],
   },
